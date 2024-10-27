1ab2d773c39482886ebe00cd0f1a7f2b66c5e027
= <=>
2024-10-27 09:04:12 +0700

    Create routes Home Movies and Movies Details

--- movie-app-fe/src/App.tsx
+++ movie-app-fe/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Details from "./pages/Details";
 
 function App() {
-  return(
+  return (
     <div className="">
-      <Navbar/>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/moviedetails/:id" element={<Details />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
