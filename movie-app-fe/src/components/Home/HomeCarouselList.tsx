4ed4adecf8c48eb5d77a9159d159346f5bcb7f11
= <=>
2024-11-07 17:49:47 +0700

    Update carousel list component

--- movie-app-fe/src/components/Home/HomeCarouselList.tsx
+++ movie-app-fe/src/components/Home/HomeCarouselList.tsx
@@ -1,22 +1,36 @@
 import { CarouselMovie } from "../../utils/constant"
 import { imagepath } from "../../utils/constant"
+import { FiThumbsUp } from "react-icons/fi"
+
 interface HomeCarouselListProps {
-    next : number[]
+    next: number[]
     carouselMovies: CarouselMovie[]
 }
 
-function HomeCarouselList({next, carouselMovies}: HomeCarouselListProps ) {
-    console.log(next) 
-  return (
-    <div>
-        <h1 className='text-yellow-500 font-bold text-xl'>Up next</h1>
-        {
-            next.map((item, index) =>(
-                <img src={imagepath + carouselMovies[item]?.poster_path } className="w-[100px]" alt="" />
-            ))
-        }
-    </div>
-  )
+function HomeCarouselList({ next, carouselMovies }: HomeCarouselListProps) {
+    console.log(next)
+    return (
+        <div>
+            <h1 className='text-yellow-500 font-bold text-xl'>Up next</h1>
+            {
+                next.map((item, index) => (
+                    <div className="flex gap-2">
+                        <img src={imagepath + carouselMovies[item]?.poster_path} className="w-[100px]" alt="" />
+                        <div className="flex flex-col justify-between py-2">
+                            <div className="leading-5">
+                                <h1>{carouselMovies[item]?.title}</h1>
+                                <h1 className="text-md text-zinc-300 line-clamp-3">{carouselMovies[item]?.overview}</h1>
+                            </div>
+                            <div className="flex gap-1 text-center ">
+                                <FiThumbsUp />
+                                <h2>{carouselMovies[item]?.vote_count}</h2>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default HomeCarouselList
