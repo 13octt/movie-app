d1626351da5ad1d8d9e376b22d1be245d79142a2
= <=>
2024-12-06 20:43:05 +0700

    Movie Details Page API Implementation

--- movie-app-fe/src/pages/Details/index.tsx
+++ movie-app-fe/src/pages/Details/index.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { baseApi } from "../../api/axiosInstance";
+
 function Details() {
-  return (
-    <div>Details</div>
-  )
+	const params = useParams()
+
+	useEffect(() => {
+		fetchMovieDetails();
+	}, [params])
+
+	const fetchMovieDetails = async () => {
+		const response = await baseApi.get(`/3/movie/${params.id}?language=en-US`)
+		console.log(response.data)
+	} 
+
+	return (
+		<div>Details</div>
+	)
 }
 
 export default Details
