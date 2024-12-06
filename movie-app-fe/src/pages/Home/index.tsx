551f42e5106ddd87660e466bb942710394ba2031
= <=>
2024-12-06 14:44:42 +0700

    Load More Movie

--- movie-app-fe/src/pages/Home/index.tsx
+++ movie-app-fe/src/pages/Home/index.tsx
@@ -7,25 +7,32 @@ import LoadMoreBtn from '../../components/Button/LoadMoreBtn'
 
 function Home() {
 	const [movies, setMovies] = useState<MovieCardType[]>([])
-	const fetchMovies = async () => {
+	const [page, setPage] = useState(1)
+	const fetchMovies = async (page: number) => {
 		try {
-			const response = await baseApi.get('/3/movie/top_rated?language=en-US&page=1')
+			const response = await baseApi.get(`/3/movie/top_rated?language=en-US&page=${page}`)
 			console.log(`popular movie`, response.data.results)
-			setMovies(response.data.results)
+			setMovies(prev => [...prev, ...response.data.results])
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
 	useEffect(() => {
-		fetchMovies()
-	}, [])
+		fetchMovies(page)
+	}, [page])
+
+	const handlePageUpdate = () => {
+		setPage(prev => prev + 1)
+	}
 
 	return (
 		<div className='w-[90%] mx-auto mb-44'>
 			<HomeSlider />
 			<MovieList movies={movies} />
-			<LoadMoreBtn />
+			<div onClick={() => { handlePageUpdate() }}>
+				<LoadMoreBtn />
+			</div>
 		</div>
 	)
 }
