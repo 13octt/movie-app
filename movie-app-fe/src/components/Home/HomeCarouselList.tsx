1ddfa25c87d2ee0cfb9e15b3a71d285495db920d
= <=>
2024-12-07 11:36:11 +0700

    Create Search List Component

--- movie-app-fe/src/components/Home/HomeCarouselList.tsx
+++ movie-app-fe/src/components/Home/HomeCarouselList.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react"
 import { CarouselMovieType } from "../../utils/constant"
-import { imagepath } from "../../utils/constant"
-import { FiThumbsUp } from "react-icons/fi"
-import { Link } from "react-router-dom"
+import CarouselMiniCard from "./CarouselMiniCard"
 
 interface HomeCarouselListProps {
     next: number[]
@@ -10,31 +7,12 @@ interface HomeCarouselListProps {
 }
 
 function HomeCarouselList({ next, carouselMovies }: HomeCarouselListProps) {
-    const [hover, setHover] = useState<number | null>(null)
     return (
         <div>
             <h1 className='text-yellow-500 font-bold text-xl'>Up next</h1>
             {
                 next.map((item, ind) => (
-                    <Link key={ind} to={`/details/${carouselMovies[item].id}`}>
-                        <div className="flex gap-2"
-                            onMouseEnter={() => setHover(ind)}
-                            onMouseLeave={() => setHover(null)}
-                        >
-
-                            <img src={imagepath + carouselMovies[item]?.poster_path} className="w-[100px]" alt="" />
-                            <div className="flex flex-col justify-between py-2">
-                                <div className="leading-5">
-                                    <h1 className={`${hover === ind ? "underline" : ""}`}>{carouselMovies[item]?.title}</h1>
-                                    <h1 className="text-md text-zinc-300 line-clamp-3">{carouselMovies[item]?.overview}</h1>
-                                </div>
-                                <div className="flex gap-1 text-center ">
-                                    <FiThumbsUp />
-                                    <h2>{carouselMovies[item]?.vote_count}</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
+                    <CarouselMiniCard carouselMovies={carouselMovies} item={item} ind={ind} />
                 ))
             }
         </div>
--- movie-app-fe/src/components/Navbar/index.tsx
+++ movie-app-fe/src/components/Navbar/index.tsx
@@ -1,6 +1,48 @@
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import CarouselMiniCard from "../Home/CarouselMiniCard"
+import { baseApi } from "../../api/axiosInstance"
+import { CarouselMovieType } from "../../utils/constant"
+import { IoClose } from "react-icons/io5"
 
 function Navbar() {
+    const [search, setSearch] = useState("")
+    const [searchedList, setSearchedList] = useState<CarouselMovieType[]>([])
+    const [showSearch, setShowSearch] = useState(false)
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
+    const toggleShow = (bool: boolean) => {
+        setShowSearch(bool)
+    }
+
+    const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            (event?.target as HTMLInputElement).blur()
+        }
+    }
+
+    const fetchSearch = async () => {
+        try {
+            const response = await baseApi.get(`/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`)
+            console.log(response)
+            setSearchedList(response.data.results)
+        } catch (error) {
+            console.log("fetch search error", error)
+        }
+    }
+
+
+    useEffect(() => {
+        if (search.length > 0)
+            setShowSearch(true)
+        else
+            setShowSearch(false)
+        fetchSearch()
+    }, [search])
+
     return (
         <nav className="bg-[#121212] py-2">
             <div className="flex justify-between items-center w-[80%] mx-auto">
@@ -18,8 +60,34 @@ function Navbar() {
                 <div className="">
                     <input
                         placeholder="search"
+                        onChange={handleChange}
+                        onKeyDown={handleKeyPress}
+                        onClick={() => toggleShow(true)}
                         className="w-[500px] h-10 bg-black text-[#c2c2c2] text-lg outline-none px-4 placeholder:text-[#646464] rounded-xl"
                         type="text" />
+                    <div className="relative">
+                        <div className="absolute z-10 left-0 w-full h-[200px]  rounded-xl">
+                            {
+                                showSearch && search.length > 0 &&
+                                <div className="absolute z-30 top-2 right-1 text-yellow-500 sm:text-2xl text-xl"
+                                    onClick={() => toggleShow(false)}
+                                ><IoClose /></div>
+                            }
+                            {
+                                showSearch && search.length > 0 && <div className="relative" onClick={() => toggleShow(false)}>
+                                    <div className="sm:absolute fixed z-50 left-0 sm:max-w-[500px] w-full bg-zinc-800 rounded-xl">
+                                        <div className="py-3 pl-5">
+                                            <div className="flex flex-col gap-2 h-fit max-h-[380px] overflow-y-auto">
+                                                {searchedList.length > 0 && searchedList.map((item, ind) =>
+                                                    <CarouselMiniCard carouselMovies={searchedList} ind={ind} item={ind} />
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    </div>
                 </div>
 
             </div>
